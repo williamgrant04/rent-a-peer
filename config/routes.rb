23d0abb0814d4e02bbdc215d8a4a06f0652a9411
Brad Dougherty <brad.doughertys@gmail.com>
2024-04-29 16:52:07 +0200

    Created articles controller with new/create/destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/destroy'
   devise_for :users
 end
---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get articles_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get articles_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get articles_destroy_url
+    assert_response :success
+  end
+end
