c721057093b06a3a505ada1bebaa98ed1163d9b1
Brad Dougherty <brad.doughertys@gmail.com>
2024-04-29 16:06:03 +0200

    added validations for names, age and location

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 17, message: 'You must be older than 18' }
+  validates :location, presence: true
 end
