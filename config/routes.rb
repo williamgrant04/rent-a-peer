af730d5a485abbcf699b85413f33adf4adfb9d78
Edgard Revol <edgardrevolr@gmail.com>
2024-04-30 15:54:20 +0200

    transferreed homepage to services controller#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'services/index'
   get 'services/show'
   devise_for :users
-  root to: 'homepage#index'
   resources :user_services, only: %i[new create destroy]
   resources :services, only: %i[index show]
 end
