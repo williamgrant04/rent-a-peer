126a3fb7b04ef2e256e9aa4c6a5eed221cb64fc4
William Grant <williamg3575467@gmail.com>
2024-04-30 16:24:14 +0100

    Create user profile page and added banner select functionality, (isn't fully complete but it's a first working version)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @services = @user.services
+    @services = []
+  end
+
+  def banner
+    @user = current_user
+    render partial: 'users/edit_banner_modal'
+  end
+
+  def update_banner
+    
   end
 end
