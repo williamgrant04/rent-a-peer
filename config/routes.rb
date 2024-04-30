6911c6ac51d2627a8d9fe1543f29b11202e12b8b
Edgard Revol <edgardrevolr@gmail.com>
2024-04-30 17:31:17 +0200

    root to: services#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'services/index'
   get 'services/show'
+  root to: 'services#index'
   devise_for :users
   resources :user_services, only: %i[new create destroy]
   resources :services, only: %i[index show]
