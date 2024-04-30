b878543148008f686b0fc9d0c3402f4665439538
Brad Dougherty <brad.doughertys@gmail.com>
2024-04-30 17:47:04 +0200

    have a new and create action that works. Needs redirect logic

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,29 @@
 class ReviewsController < ApplicationController
+  before_action :set_user_service, only: %i[create]
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.user_service = @user_service
+    @review.save
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rating)
+  end
+
+  def set_user_service
+    @user_service = UserService.find(params[:user_service_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'services/index'
   get 'services/show'
   devise_for :users
-  resources :user_services, only: %i[new create destroy]
+  resources :user_services, only: %i[new create destroy] do
+    resources :reviews, only: %i[new create destroy]
+  end
   resources :services, only: %i[index show]
 end
