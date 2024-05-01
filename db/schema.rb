2cb480dae86ed2ddd6bc2798b5f2db54dfe53bfd
William Grant <williamg3575467@gmail.com>
2024-05-01 10:00:19 +0100

    Update migrations (oopsie)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_01_070857) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_01_085634) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,15 +43,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_070857) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "user_service_id", null: false
     t.bigint "user_id", null: false
-    t.string "title"
-    t.text "description"
-    t.float "hourly_rate"
+    t.date "start_date"
+    t.date "end_date"
+    t.boolean "accepted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
-    t.index ["user_service_id"], name: "index_bookings_on_user_service_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -99,7 +97,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_01_070857) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "user_services"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "user_services"
   add_foreign_key "reviews", "users"
