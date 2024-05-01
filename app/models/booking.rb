712afc10b5bc726bf94567fe7aed2b5618450324
William Grant <williamg3575467@gmail.com>
2024-05-01 10:38:14 +0100

    Fill bookings model and add temp routes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,19 @@
 class Booking < ApplicationRecord
+  belongs_to :user
+  # belongs_to :listing
+
+  validates :start_date, presence: true
+  validate :start_date_cannot_be_past, on: :create
+
+  validates :end_date, presence: true
+  validates :user_id, presence: true
+  # validates :listing_id, presence: true
+
+  private
+
+  def start_date_cannot_be_past
+    if start_date.present? && start_date < Date.today
+      errors.add(:start_date, "can't be in the past")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :user_services, only: %i[new create destroy] do
     resources :reviews, only: %i[new create destroy]
   end
-  resources :services, only: %i[index show]
+  resources :services, only: %i[show]
   get "profile", to: "users#show"
   get "banner-partial", to: "users#banner"
+  resources :bookings, only: %i[new create edit update] # Should be nested in listings
+  resources :bookings, only: %i[destroy]
 end
