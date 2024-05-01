b8420873b46e33e8d0dbf3690e4f89b7d5618539
Brad Dougherty <brad.doughertys@gmail.com>
2024-05-01 15:36:50 +0200

    refactored show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @services = []
-    @user.services.each { |service| @services << service }
+    @services = @user.services
   end
 
   def banner
