c46c84afc842e9b838ecca9e82370d9aa910546b
William Grant <williamg3575467@gmail.com>
2024-05-01 10:50:39 +0100

    Add flatpickr and connect it to new booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,21 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    debugger
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    debugger
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
   end
 end
