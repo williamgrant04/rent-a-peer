89a41f51bdc7346aafea8464973b378423d91209
William Grant <williamg3575467@gmail.com>
2024-05-02 11:49:36 +0100

    Update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @services = @user.services
+    @bookings = @user.bookings
+    @listings = @user.listings
   end
 
   def profile
     @user = current_user
     @services = @user.services
+    @listings = @user.listings
+    @bookings = @user.bookings
   end
 
   def banner
--- app/models/user_service.rb
+++ app/models/user_service.rb
@@ -1,5 +1,7 @@
 class UserService < ApplicationRecord
   belongs_to :user
   belongs_to :service
+
+  has_many :listings
   has_many :reviews
 end
